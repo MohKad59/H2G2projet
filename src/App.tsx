271371b2034d1fdf1950a6dc24676ec0d079bf56
Mohamed Kaddour <kadnakamo@gmail.com>
2025-02-06 17:15:08 +0100

    add Videobackground

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import CardShop from "./components/cardShop/cardShop/CardShop";
+import VideoBackground from "./components/VideoBackground";
 
 function App() {
-	const [count, setCount] = useState(0);
+	// biome-ignore lint/correctness/noEmptyPattern: <explanation>
+	const [] = useState(0);
 
 	return (
 		<>
+			<VideoBackground />
 			<CardShop />
 		</>
 	);
