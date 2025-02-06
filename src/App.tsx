f0d06fb51e82add6ed875c6cac2464d9ea21a470
erwan lorand <erwan.lorand@protonmail.com>
2025-02-06 21:01:45 +0100

    resolution conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import CardShop from "./Components/cardShop";
-import Galerie from "./Components/ComponantGalerie/Galerie";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+// import CardShop from "./Components/cardShop";
+// import Galerie from "./Components/ComponantGalerie/Galerie";
 
-function App() {
-	const [count, setCount] = useState(0);
+// function App() {
+// 	const [count, setCount] = useState(0);
 
-	return (
-		<>
-			<Galerie />
-			<CardShop />
-		</>
-	);
-}
+// 	return (
+// 		<>
+// 			<Galerie />
+// 			<CardShop />
+// 		</>
+// 	);
+// }
 
-export default App;
+// export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,44 +1,44 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import "./index.css";
-import Home from "./Pages/home.tsx";
-import App from "./App.tsx";
-import Galerie from "./Components/ComponantGalerie/Galerie.tsx";
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// import "./index.css";
+// import Home from "./Pages/home.tsx";
+// import App from "./App.tsx";
+// import Galerie from "./Components/ComponantGalerie/Galerie.tsx";
 
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
-// Create router configuration with routes
-// You can add more routes as you build out your app!
-const router = createBrowserRouter([
-	{
-		element: <App />,
-		children: [
-			{
-				path: "/",
-				element: </>,
-			},
-			{
-				path: "/Galerie",
-				element: <Galerie />,
-			},
-		],
-	},
-]);
-// Find the root element in the HTML document
-const rootElement = document.getElementById("root");
-if (rootElement == null) {
-	throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
-}
+// createRoot(document.getElementById("root")!).render(
+// 	<StrictMode>
+// 		<App />
+// 	</StrictMode>,
+// );
+// // Create router configuration with routes
+// // You can add more routes as you build out your app!
+// const router = createBrowserRouter([
+// 	{
+// 		element: <App />,
+// 		children: [
+// 			// {
+// 			// 	path: "/",
+// 			// 	element: </>,
+// 			// },
+// 			// {
+// 			// 	path: "/Galerie",
+// 			// 	element: <Galerie />,
+// 			// },
+// 		],
+// 	},
+// ]);
+// // Find the root element in the HTML document
+// const rootElement = document.getElementById("root");
+// if (rootElement == null) {
+// 	throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
+// }
 
-// Render the app inside the root element
-createRoot(rootElement).render(
-	<StrictMode>
-		{" "}
-		{/* Wrap the App component with CartProvider */}
-		<RouterProvider router={router} />
-	</StrictMode>,
-);
+// // Render the app inside the root element
+// createRoot(rootElement).render(
+// 	<StrictMode>
+// 		{" "}
+// 		{/* Wrap the App component with CartProvider */}
+// 		<RouterProvider router={router} />
+// 	</StrictMode>,
+// );
