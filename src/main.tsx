6e3a9c115518c2d4076a3844e07f334aaf01c642
Mohamed Kaddour <kadnakamo@gmail.com>
2025-02-06 17:05:51 +0100

    first commit Accueil

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import VideoBackground from "./components/VideoBackground.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		element: <App />,
+		errorElement: <div>Something went wrong! Please try again later.</div>, // Message d'erreur global
+		children: [
+			{
+				path: "/videobackground",
+				element: <VideoBackground />,
+				errorElement: <div>Error loading video background.</div>, // Message d'erreur spécifique à cette route
+			},
+			// Ajoutez d'autres routes ici si nécessaire
+		],
+	},
+]);
+
+const rootElement = document.getElementById("root");
+if (rootElement) {
+	createRoot(rootElement).render(
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>,
+	);
+} else {
+	console.error("Root element not found");
+}
