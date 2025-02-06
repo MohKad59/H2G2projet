74319f56bedb0d6862e79e7eec6cbbece6888839
Mohamed Kaddour <kadnakamo@gmail.com>
2025-02-06 23:45:15 +0100

    add Navbar Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,28 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Accueil from "./components/Accueil/Accueil";
 import Panier from "./components/Panier/Panier";
 import Contact from "./components/Contact/Contact";
 import Paiement from "./components/Paiement/Paiement";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import VideoBackground from "./components/Accueil/VideoBackground";
 
 function App() {
 	return (
 		<Router>
-			<div>
-				<nav>
-					<ul>
-						<li>
-							<Link to="/">Accueil</Link>
-						</li>
-						<li>
-							<Link to="/panier">Panier</Link>
-						</li>
-						<li>
-							<Link to="/contact">Contact</Link>
-						</li>
-						<li>
-							<Link to="/paiement">Paiement</Link>
-						</li>
-					</ul>
-				</nav>
-				<Routes>
-					<Route path="/" element={<Accueil />} />
-					<Route path="/panier" element={<Panier />} />
-					<Route path="/contact" element={<Contact />} />
-					<Route path="/paiement" element={<Paiement />} />
-				</Routes>
+			<div className="app-container">
+				<VideoBackground /> {/* La vidéo est rendue ici */}
+				<Navbar /> {/* La navbar est fixée en haut */}
+				<div className="content-container">
+					<Routes>
+						<Route path="/" element={<Accueil />} />
+						<Route path="/panier" element={<Panier />} />
+						<Route path="/contact" element={<Contact />} />
+						<Route path="/paiement" element={<Paiement />} />
+					</Routes>
+				</div>
+				<Footer /> {/* Le footer reste en bas */}
 			</div>
 		</Router>
 	);
